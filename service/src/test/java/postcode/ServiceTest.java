2ce1d3e4223bf3743a39dca91073f02e68de059b
Matt <Matt@Maximus>
2024-04-08 18:21:51 +0100

    Update tests

--- service/src/test/java/postcode/ServiceTest.java
+++ service/src/test/java/postcode/ServiceTest.java
@@ -1,5 +1,7 @@
 package postcode;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Optional;
@@ -10,7 +12,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.elasticsearch.client.elc.ElasticsearchTemplate;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.testcontainers.elasticsearch.ElasticsearchContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -18,15 +22,21 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 import postcode.documents.Ward;
 import postcode.repos.WardRepo;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
 @Testcontainers
 public class ServiceTest {
 
-	@Autowired
-	private ElasticsearchTemplate elasticsearchTemplate;
+	// @Autowired
+	// private ElasticsearchTemplate elasticsearchTemplate;
+
+	@LocalServerPort
+	private int port;
 
 	@Autowired
 	private WardRepo wardRepo;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
 
 	@Container
 	public static ElasticsearchContainer elasticsearchContainer = new PostcodeElasticsearchContainer();
@@ -55,5 +65,12 @@ public class ServiceTest {
 	void testCRUD() {
 		Optional<Ward> ward = wardRepo.findById("WardId");
 		assertTrue(ward.isPresent());
+	}
+
+	@Test
+	void greetingShouldReturnDefaultMessage() throws Exception {
+		Ward testWard = this.restTemplate.getForObject("http://localhost:" + port + "/ward/WardId", Ward.class);
+		assertThat(testWard.getWd22cd(), equalTo("WardId"));
+		assertThat(testWard.getWd22nm(), equalTo("WardName"));
 	}
 }
