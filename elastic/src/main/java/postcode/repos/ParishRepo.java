868661fa608b29e0b934583832e16a8ee518ecb7
hardym <matthew.hardy@ynap.com>
2024-04-25 12:48:30 +0100

    parish fuzz = 0

--- elastic/src/main/java/postcode/repos/ParishRepo.java
+++ elastic/src/main/java/postcode/repos/ParishRepo.java
@@ -2,10 +2,13 @@ package postcode.repos;
 
 import java.util.Optional;
 
+import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 import postcode.documents.Parish;
 
 public interface ParishRepo extends ElasticsearchRepository<Parish, String> {
+
+	@Query("{\"match\": {\"PARNCP21CD\": {\"query\":\"?0\", \"fuzziness\": \"0\"} }}")
 	public Optional<Parish> findByPARNCP21CD(String pARNCP21CD);
 }
